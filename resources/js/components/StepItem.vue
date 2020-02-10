caa2d359b03856a54322e37544e2f5f4096007d6
paruko <hrk.0u0x@gmail.com>
2020-02-11 02:58:12 +0900

    backup

--- resources/js/components/StepItem.vue
+++ resources/js/components/StepItem.vue
@@ -5,8 +5,8 @@
       <span class="p-rowpost-category">{{ step.category.name }}</span>
       <h4 class="p-rowpost-title">{{ step.title }}時間</h4>
       <div class="p-rowpost-info">
-        <span class="p-rowpost-info__time"><span class="c-icon"><img src="img/common/ico_clock.svg" alt=""></span>目安学習期間：{{ step.time }}時間</span>
-        <span class="p-rowpost-info__author"><span class="c-icon"><img src="img/common/ico_author.svg" alt=""></span>作成者：{{ step.user.name }}</span>
+        <span class="p-rowpost-info__time"><span class="c-icon"><img src="/img/common/ico_clock.svg" alt=""></span>目安学習期間：{{ step.time }}時間</span>
+        <span class="p-rowpost-info__author"><span class="c-icon"><img src="/img/common/ico_author.svg" alt=""></span>作成者：{{ step.user.name }}</span>
       </div>
       <p class="p-rowpost-txt">{{ step.info }}</p>
     </div>
