cbf16b8c9b33454c04ef1949f809478d9cabfc20
paruko <hrk.0u0x@gmail.com>
2020-02-29 18:48:17 +0900

    header修正

--- resources/views/components/header-index.blade.php
+++ resources/views/components/header-index.blade.php
@@ -1,30 +1,35 @@
 <header class="l-header">
     <div class="p-header">
-      <div class="p-header-titleWrapper">
-        <a href="/">
-          <h1 class="p-header-title"><img src="{{ asset('img/common/ico_sitelogo.svg') }}" alt=""></h1>
-          <span class="p-header-subtitle">あなたの人生のSTEPを<br>共有しよう</span>
-        </a>
+      <div class="p-header-top">
+        <div class="p-header-titleWrapper">
+          <a href="/">
+            <h1 class="p-header-title"><img src="{{ asset('img/common/ico_sitelogo.svg') }}" alt=""></h1>
+            <span class="p-header-subtitle">あなたの人生のSTEPを<br>共有しよう</span>
+          </a>
+        </div>
+        {{-- pc menu --}}
+        <nav class="p-header-pcMenu u-display-pc js-index-menu">
+          <ul>
+            <li><a href="#step01">STEPとは</a></li>
+            <li><a href="#step02">機能と特徴</a></li>
+            <li><a href="#step03">人気のSTEP</a></li>
+          </ul>
+        </nav>
       </div>
-      {{-- pc menu --}}
-      <nav class="p-header-pcMenu u-display-pc js-index-menu">
-        <ul>
-          <li><a href="#step01">STEPとは</a></li>
-          <li><a href="#step02">機能と特徴</a></li>
-          <li><a href="#step03">人気のSTEP</a></li>
-        </ul>
-      </nav>
-      <div class="p-header-btns u-display-pc">
-        <ul>
-          @guest
-            <li class="c-btn p-header-btn as_login">
-              <a href="{{ route('login') }}">ログイン</a>
-            </li>
-            <li class="c-btn p-header-btn as_signup">
-              <a href="{{ route('register') }}">新規登録</a>
-            </li>
-          @endauth
-        </ul>
+
+      <div class="p-header-bottom">
+        <div class="p-header-btns u-display-pc">
+          <ul>
+            @guest
+              <li class="c-btn p-header-btn as_login">
+                <a href="{{ route('login') }}">ログイン</a>
+              </li>
+              <li class="c-btn p-header-btn as_signup">
+                <a href="{{ route('register') }}">新規登録</a>
+              </li>
+            @endauth
+          </ul>
+        </div>
       </div>
   
       {{-- sp menu --}}
