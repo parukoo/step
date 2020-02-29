cda486681563c3e679be78ca92f7bb6d078b77ac
paruko <hrk.0u0x@gmail.com>
2020-02-29 18:41:15 +0900

    header修正

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,55 +1,61 @@
 <header class="l-header">
     <div class="p-header">
-      <div class="p-header-titleWrapper">
-        <a href="/">
-          <h1 class="p-header-title"><img src="{{ asset('img/common/ico_sitelogo.svg') }}" alt=""></h1>
-          <span class="p-header-subtitle">あなたの人生のSTEPを<br>共有しよう</span>
-        </a>
-      </div>
-      {{-- pc menu --}}
-      <nav class="p-header-pcMenu u-display-pc">
-        <ul>
-          <li><a href="{{ route('steps')}}">STEP一覧</a></li>
-          <li class="js-category-menu p-header-category">カテゴリー検索<i class="p-header-category__icon fas fa-sort-down"></i>
-            <ul class="p-header-category-menu  as_login">
-              @foreach($categories as $category)
-                <li><a href="/category/{{ $category->id }}">・{{ $category->name }}</a></li>
-              @endforeach
+      <div class="p-header-top">
+        <div class="p-header-titleWrapper">
+            <a href="/">
+              <h1 class="p-header-title"><img src="{{ asset('img/common/ico_sitelogo.svg') }}" alt=""></h1>
+              <span class="p-header-subtitle">あなたの人生のSTEPを<br>共有しよう</span>
+            </a>
+          </div>
+    
+          {{-- pc menu --}}
+          <nav class="p-header-pcMenu u-display-pc">
+            <ul>
+              <li><a href="{{ route('steps')}}">STEP一覧</a></li>
+              <li class="js-category-menu p-header-category">カテゴリー検索<i class="p-header-category__icon fas fa-sort-down"></i>
+                <ul class="p-header-category-menu  as_login">
+                  @foreach($categories as $category)
+                    <li><a href="/category/{{ $category->id }}">・{{ $category->name }}</a></li>
+                  @endforeach
+                </ul>
+              </li>
+              <li><a href="{{ route('steps.new')}}">STEPを作る</a></li>
             </ul>
-          </li>
-          <li><a href="{{ route('steps.new')}}">STEPを作る</a></li>
-        </ul>
-      </nav>
-      <div class="p-header-btns u-display-pc">
-        <ul>
-          @auth
-            <li class="p-header-btn__mypage as_login">
-              <a href="{{ route('mypage') }}">
-                @if (Auth::user()->photo)
-                  <figure class="p-avator as_small"><img src="{{ asset('img/update/user/'.Auth::user()->photo) }}.jpg" alt=""></figure>
-                @else
-                  <figure class="p-avator as_small"><img src="{{ asset('img/common/img_noimage.jpg') }}" alt="*"></figure>
-                @endif
-                <span class="p-avator__name">{{  Auth::user()->name }}</span>
-              </a>
-            </li>
-            <li class="c-btn p-header-btn as_logout">
-              <a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">ログアウト</a>
-            </li>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-            {{ csrf_field() }}
-            </form>
-          @endauth
-  
-          @guest
-            <li class="c-btn p-header-btn as_login">
-              <a href="{{ route('login') }}">ログイン</a>
-            </li>
-            <li class="c-btn p-header-btn as_signup">
-              <a href="{{ route('register') }}">新規登録</a>
-            </li>
-          @endauth
-        </ul>
+          </nav>
+      </div>
+
+      <div class="p-header-bottom">
+        <div class="p-header-btns u-display-pc">
+          <ul>
+            @auth
+              <li class="p-header-btn__mypage as_login">
+                <a href="{{ route('mypage') }}">
+                  @if (Auth::user()->photo)
+                    <figure class="p-avator as_small"><img src="{{ asset('img/update/user/'.Auth::user()->photo) }}.jpg" alt=""></figure>
+                  @else
+                    <figure class="p-avator as_small"><img src="{{ asset('img/common/img_noimage.jpg') }}" alt="*"></figure>
+                  @endif
+                  <span class="p-avator__name">{{  Auth::user()->name }}</span>
+                </a>
+              </li>
+              <li class="c-btn p-header-btn as_logout">
+                <a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">ログアウト</a>
+              </li>
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              {{ csrf_field() }}
+              </form>
+            @endauth
+    
+            @guest
+              <li class="c-btn p-header-btn as_login">
+                <a href="{{ route('login') }}">ログイン</a>
+              </li>
+              <li class="c-btn p-header-btn as_signup">
+                <a href="{{ route('register') }}">新規登録</a>
+              </li>
+            @endauth
+          </ul>
+        </div>
       </div>
   
       {{-- sp menu --}}
